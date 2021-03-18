943d67e6625588e817329cd2328c6e81f1dc94e4
István Bence Herczegh <istvan.herczegh@stud.uni-corvinus.hu>
2021-03-18 11:33:09 +0100

    js gyak probalkozas 1

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,11 @@
+﻿window.onload = function () {
+    let hova = document.getElementById("ide");
+    document.getElementById("ide").innerHTML = '';
+    for (var i = 1; i < 11; i++) {
+        var ujDiv = document.createElement("div");
+        ujDiv.classList.add("sorban");
+        ujDiv.id = i;
+        ujDiv.innerText = i;
+        hova.appendChild(ujDiv);
+    }
+}
